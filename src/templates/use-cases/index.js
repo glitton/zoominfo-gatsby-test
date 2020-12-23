48a73545252cfe75a18ad133b4c665b9f69f87c5
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2020-12-22 16:53:13 -0800

    add in use case with a extension in the name

---
+++ src/templates/use-cases/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const UseCase = () => (
+  <Layout>
+    <SEO title="Use Cases" />
+    <h1>A use case</h1>
+  </Layout>
+)
+
+export default UseCase
