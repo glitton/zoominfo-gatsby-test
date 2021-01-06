f0d5487f112faee830d0ca61eae6f98ff4496f18
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2021-01-06 07:58:52 -0800

    upgrade and add functions

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-functions`,
+      options: {
+        path: `${__dirname}/src/api`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
