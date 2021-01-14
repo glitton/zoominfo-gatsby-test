423fd92a3b566711ab5c984365b51f40fcaf9d45
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2021-01-14 13:55:06 -0800

    add my name

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
+      <h1>Dustin McCraw,</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
       <p>Built on {process.env.__BUILD_DATE__}</p>
