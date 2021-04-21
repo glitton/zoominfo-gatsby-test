c56be8a94a4c7ae1856ac314349e62f7ffa1b716
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2021-04-21 11:02:35 -0700

    add debug.js

---
+++ src/api/debug.js
@@ -0,0 +1,14 @@
+module.exports = (req, res) => {
+  res.status(200).send({
+    host: req.host,
+    hostname: req.hostname,
+    ip: req.ip,
+    ips: req.ips,
+    params: req.params,
+    path: req.path,
+    protocol: req.protocol,
+    query: req.query,
+    headers: req.headers,
+    body: req.body,
+  })
+}
