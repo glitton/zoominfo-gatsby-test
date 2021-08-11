4ab3e68abcc7ffc6e4e1745b1a31674efbf2d8bd
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2021-08-11 15:19:03 -0700

    remove functions flag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  flags: {
-    FUNCTIONS: true,
-  },
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
