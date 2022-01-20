3ddce851c879c7cb4d903bb09a085fe5e096ec47
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2022-01-20 07:19:28 -0800

    add 403 and 500 custom pages

---
+++ src/pages/403.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <Seo title="403: Forbidden" />
+    <h1>403: Forbidden</h1>
+  </Layout>
+)
+
+export default NotFoundPage
---
+++ src/pages/500.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <Seo title="500: ServerError" />
+    <h1>500: ServerError</h1>
+  </Layout>
+)
+
+export default NotFoundPage
