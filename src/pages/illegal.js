6ae225f91fcb86e0e1cd3baf65640ac9adba21a1
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2022-01-20 16:51:54 -0800

    add illegal.js

---
+++ src/pages/illegal.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IllegalPage = () => (
+  <Layout>
+    <Seo title="Illegal" />
+    <h1>Can't see this page in the US</h1>
+  </Layout>
+)
+
+export default AboutPage
