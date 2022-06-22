93ca7af3e723c61f040bda91c027fbc2b84dbe08
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2022-06-22 16:26:49 -0700

    set headers on the response for api

--- src/api/debug.js
+++ src/api/debug.js
@@ -1,4 +1,6 @@
 module.exports = (req, res) => {
+  res.headers[`x-site-id`] = `dustins-site`
+  res.headers[`x-dustin-test`] = `mccraw`
   res.status(200).send({
     host: req.host,
     hostname: req.hostname,
